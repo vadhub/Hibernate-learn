da709eac4dfef78066472793ccd4cf90b108bc95
Vadim <gabderahmanov99@gmail.com>
2019-07-17 15:54:41 +0400

    create delete class

--- src/com/vg/onetoone/OneToOneDelete.java
+++ src/com/vg/onetoone/OneToOneDelete.java
@@ -7,26 +7,23 @@ import org.hibernate.cfg.Configuration;
 import com.vg.hibernate.entity.Instructor;
 import com.vg.hibernate.entity.InstructorDetail;
 
-public class OneToOne {
+public class OneToOneDelete {
 
 	public static void main(String[] args) {
 	SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class).addAnnotatedClass(InstructorDetail.class).buildSessionFactory();
 		
 		Session session = factory.getCurrentSession();
 		
-		//create objects
-		
-		Instructor instructor = new Instructor("Nill", "Ams");
-		InstructorDetail inDetail1 = new InstructorDetail("VG", "disignerr");
-		
-		instructor.setInstructorDetail(inDetail1);
-		
 		session.beginTransaction();		
 		
-		System.out.println("Save!");
-		session.save(instructor);
+		//get instructor by primary key
+		Instructor tempInstructor = session.get(Instructor.class, 2);
 		
-		System.out.println("Save! WoooHooo!");
+		//delete instructor
+		if(tempInstructor != null) {
+			session.delete(tempInstructor);
+			System.out.println("Delete! WoooHooo!");
+		}
 		
 		session.getTransaction().commit();
 		

17f46040a395ee520d328f4a9f76f39d46a3bb89
Vadim <gabderahmanov99@gmail.com>
2019-07-18 14:07:49 +0400

    bidirection get instructorDetail

---
+++ src/com/vg/onetoone/BiDirectionRunner.java
@@ -0,0 +1,32 @@
+package com.vg.onetoone;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.vg.hibernate.entity.Instructor;
+import com.vg.hibernate.entity.InstructorDetail;
+
+public class BiDirectionRunner {
+
+	public static void main(String[] args) {
+	SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class).addAnnotatedClass(InstructorDetail.class).buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		
+		session.beginTransaction();		
+	
+		int theId = 4;
+		
+		InstructorDetail instDetail = session.get(InstructorDetail.class, theId);
+		
+		System.out.println("instucter associate "+ instDetail.getInstructor());
+		
+		session.getTransaction().commit();
+		
+		factory.close();
+		
+		System.out.println("Woohoo");
+	}
+
+}
